6b45ff14e3b014182c60e515a94e251abf4d215b
Manish Kumar <manishkumarbodhgaya1157@gmail.com>
2025-10-28 12:30:17 +0530

    fix: hardcode backend API URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-   export const axiosInstance = axios.create({
-    baseURL: import.meta.env.Mode==="development"?"http://localhost:3000/api/v1":"/api/v1", 
-      withCredentials: true
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api/v1` : "/api/v1",
+  withCredentials: true
 });
-
 
 
